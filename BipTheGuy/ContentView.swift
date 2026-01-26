90d65abb206b1aa562527bb1ed97655f3aa648e5
codeVillier <odevill@icloud.com>
2026-01-26 16:58:56 +0200

    Ch. 3.3 PhotoPicker in SwiftUI, loadTransferrable, plus try, try? and try! (2025) by Prof. Gallaugher SwiftUI Course

--- BipTheGuy/ContentView.swift
+++ BipTheGuy/ContentView.swift
@@ -7,16 +7,19 @@
 
 import SwiftUI
 import AVFAudio
+import PhotosUI
 
 struct ContentView: View {
     @State private var audioPlayer: AVAudioPlayer!
     @State private var isFullSize = true
+    @State private var selectedPhoto: PhotosPickerItem?
+    @State private var bipImage = Image("clown")
     
     var body: some View {
         VStack {
             Spacer()
             
-            Image("clown")
+            bipImage
                 .resizable()
                 .scaledToFit()
                 .scaleEffect(isFullSize ? 1.0 : 0.9)
@@ -31,12 +34,19 @@ struct ContentView: View {
             
             Spacer()
             
-            Button {
-                // TODO: Button action here
-            } label: {
+            PhotosPicker(selection: $selectedPhoto) {
                 Label("Photo Library", systemImage: "photo.fill.on.rectangle.fill")
             }
-
+            .onChange(of: selectedPhoto) {
+                Task {
+                    guard let selectedImage = try? await selectedPhoto?.loadTransferable(type: Image.self) else {
+                        print("😡ERROR: Could not get Image from laodTransferable")
+                        return
+                    }
+                    bipImage = selectedImage
+                }
+            }
+            
 
         }
         .padding()
